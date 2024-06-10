c72d776ba269d7e6cf5fb54106fdcbc814eecee1
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-06-10 16:33:52 -0400

    Add loading state during data fetch.

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 //Hoang Nguyen
 //U12840485
 
-async function call() {    
-    try { //Handling errors and exeptions
-        const request = await fetch('https://www.course-api.com/react-store-products');
-        const data = await request.json();
-        
-        document.querySelector('#productName').textContent = `Product: ${data[0]['name']}`;
-        document.querySelector('#productImage').src = `${data[0].image}`;
-        document.querySelector('#productImage').width = 300;
-        document.querySelector('#productPrice').textContent = `Price: $${data[0].price}`;
-        document.querySelector('#productDescription').textContent = `Description: ${data[0]['description']}`
-    } catch (error) {
-        alert('There was an error loading products. Please try again later.')
+document.addEventListener('DOMContentLoaded', () => {
+
+    const loadingIndicator = document.getElementById('loading');
+
+    async function call() {    
+        try { //Handling errors and exeptions
+            // Show loading indicator
+            loadingIndicator.style.display = 'block';
+            
+            const request = await fetch('https://www.course-api.com/react-store-products');
+            const data = await request.json();
+
+            //Hide loading indicator
+            loadingIndicator.style.display = 'none';
+            
+            //Display product data
+            document.querySelector('#productName').textContent = `Product: ${data[0]['name']}`;
+            document.querySelector('#productImage').src = `${data[0].image}`;
+            document.querySelector('#productImage').width = 300;
+            document.querySelector('#productPrice').textContent = `Price: $${data[0].price}`;
+            document.querySelector('#productDescription').textContent = `Description: ${data[0]['description']}`
+        } catch (error) {   
+            //Hide loading indicator when an error occurs    
+            loadingIndicator.style.display = 'none';   
+            alert('There was an error loading products. Please try again later.')
+            
+        }
     }
-}
-call()
+
+    call()
+
+})
+
+
