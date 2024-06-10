691c42038d8c2710061038e0824f19773e2405f1
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-06-10 16:16:02 -0400

    Implement error handling for data fetching.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 //Hoang Nguyen
 //U12840485
 
-function call() {
-    const request = fetch('https://www.course-api.com/react-store-products').then((response) => {
-        return response.json();
-    }).then((data) => {
-        console.log(data);
+async function call() {    
+    try { //Handling errors and exeptions
+        const request = await fetch('https://www.course-api.com/react-store-products');
+        const data = await request.json();
+        
         document.querySelector('#productName').textContent = `Product: ${data[0]['name']}`;
         document.querySelector('#productImage').src = `${data[0].image}`;
         document.querySelector('#productImage').width = 300;
         document.querySelector('#productPrice').textContent = `Price: $${data[0].price}`;
-        document.querySelector('#productDescription').textContent = `Description: ${data[0]['description']}`;
-    })
+        document.querySelector('#productDescription').textContent = `Description: ${data[0]['description']}`
+    } catch (error) {
+        alert('There was an error loading products. Please try again later.')
+    }
 }
-
+call()
